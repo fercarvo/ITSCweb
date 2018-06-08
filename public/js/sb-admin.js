b0669c484b4a03308dd15eac5ded5846288be42d
Fernando Carvajal <efcu93@gmail.com>
2018-06-08 12:16:12 -0500

    https y error en JS web

--- public/js/sb-admin.js
+++ public/js/sb-admin.js
@@ -24,14 +24,14 @@
     e.preventDefault();
   });
   // Scroll to top button appear
-  $(document).scroll(function() {
+  /*$(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
     }
-  });
+  });*/
   // Configure tooltips globally
   $('[data-toggle="tooltip"]').tooltip()
   // Smooth scrolling using jQuery easing
